1ba3131ddebbb6540b5ed4f4a7870a6ba9b47688
Carlos Ocampo <charlie@carlosocampo.ca>
2022-08-16 23:12:33 -0500

    updated port and added test rout

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require('cors')
+const PORT = 8080
 
 // Middleware
 app.use(express.json())
@@ -9,6 +10,10 @@ app.use(cors())
 const gameRoute = require('./routes/game')
 app.use('/game', gameRoute)
 
-app.listen(8080, () => {
-    console.log("Server is listening on port 8080");
+app.get('/', (_req, res) => {
+    res.send('server is running')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
